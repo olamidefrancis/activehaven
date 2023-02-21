aa233dbe474595b6b0ce1b788e22d3cb7db227b1
abiona01 <quadratwemimo@gmail.com>
2023-02-21 21:50:14 +0100

    chore: installed necessary packages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React, {Component } from "react";
+import React, { Component } from 'react';
 import Navigation from './Components/Navigation/Navigation';
 import Application from './Components/Application/Application';
 import Slide from './Components/Slide/Slide';
@@ -9,58 +9,49 @@ import Signin from './Components/Signin/Signin';
 import Register from './Components/Register/Register';
 import Form from './Components/Form/Form';
 import 'tachyons';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state={
+    this.state = {
       route: 'home',
-    }
+    };
   }
 
-
-//connecting with server
-componentDidMount(){
-  fetch('http://localhost:3000/')
-  .then(response=>response.json())
-  .then(console.log)
-}
-
-  onRouteChange=(route)=>{
-    
-    this.setState({route: route});
+  //connecting with server
+  componentDidMount() {
+    fetch('http://localhost:3000/')
+      .then((response) => response.json())
+      .then(console.log);
   }
 
+  onRouteChange = (route) => {
+    this.setState({ route: route });
+  };
 
-
-
- render(){
-  
+  render() {
     return (
-      <div className="App">
-        
-      
-        { this.state.route==='home'?
-            <div>
-             <Navigation onRouteChange={this.onRouteChange} />
-         <Slide />
-         <Application onRouteChange={this.onRouteChange} />
-         <Contacts />
-            </div>
-         :(
-          this.state.route==='form'?
-          <Form onRouteChange={this.onRouteChange}/>
-          :this.state.route==='stafflog'?
-            <div>
-            <Signin  onRouteChange={this.onRouteChange} />
-            <Register onRouteChange={this.onRouteChange}/>
-            </div>
-          :this.state.route==='clock'?
-          <Clock onRouteChange={this.onRouteChange}/>:
-          this.setState={route:'home'}
-
-         )
-       } 
+      <div className='App'>
+        {this.state.route === 'home' ? (
+          <div>
+            <Navigation onRouteChange={this.onRouteChange} />
+            <Slide />
+            <Application onRouteChange={this.onRouteChange} />
+            <Contacts />
+          </div>
+        ) : this.state.route === 'form' ? (
+          <Form onRouteChange={this.onRouteChange} />
+        ) : this.state.route === 'stafflog' ? (
+          <div>
+            <Signin onRouteChange={this.onRouteChange} />
+            <Register onRouteChange={this.onRouteChange} />
+          </div>
+        ) : this.state.route === 'clock' ? (
+          <Clock onRouteChange={this.onRouteChange} />
+        ) : (
+          (this.setState = { route: 'home' })
+        )}
       </div>
     );
   }
